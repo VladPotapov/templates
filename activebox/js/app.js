8b644884845acacbee1aca52395b75afcba3f02b
VladPotapov <kotelnikovrm@gmail.com>
2020-05-30 12:59:10 +0800

    адаптировал activebox и подключил jquery, app.js

---
+++ activebox/js/app.js
@@ -0,0 +1,14 @@
+$(function () {
+    let header = $('#header');
+    let intro = $('#intro');
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+
+    $(window).on("scroll", function () {
+        scrollPos = $(this).scrollTop();
+        console.log(scrollPos);
+    });
+
+    header.addClass("fixed");
+});
