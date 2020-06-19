191843015122d40b3624030b97817562a02959d6
VladPotapov <kotelnikovrm@gmail.com>
2020-06-19 21:54:12 +0800

    add slider slick

--- activebox/js/app.js
+++ activebox/js/app.js
@@ -4,31 +4,64 @@ $(function () {
     let intro = $('#intro');
     let introH = intro.innerHeight();
     let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    checkScroll(scrollPos, introH);
 
 
-    $(window).on("scroll load resize", function () {
+    $(window).on("scroll resize", function () {
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
         if (scrollPos > introH) {
             header.addClass("fixed");
         }
         else {
             header.removeClass("fixed");
         }
-    });
+    }
 
     /**плавная прокрутка */
     $("[data-scroll]").on("click", function (event) {
+        //отменяет стандартное поведение
         event.preventDefault();
 
         let elementId = $(this).data('scroll');
         let elementOffset = $(elementId).offset().top;
 
-        console.log(elementOffset);
+        nav.removeClass("show");
 
         $("html, body").animate({
-            scrollTop: elementOffset
-        });
+            scrollTop: elementOffset - 70
+        }, 1000);
+    });
+
+
+    /**navToggle
+     * раскрывающиеся меню
+     */
+    navToggle.on("click", function (event) {
+        event.preventDefault();
+        nav.toggleClass("show");
+    });
+
+    /**Reviews:
+     * for slider
+     * librery  slick
+     */
+    let slider = $("#reviewsSlider");
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
     });
 
 });
